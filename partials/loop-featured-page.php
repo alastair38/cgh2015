9e06dcd77a9468107ce36fb7a25443daa5e0f2f3
alastair38 <alastair@alastaircox.com>
2015-05-17 21:09:50 +0100

    Tweaked homepage loops to set featured image as background for text areas. On hover, the text now sets opacity to 1.

--- partials/loop-featured-page.php
+++ partials/loop-featured-page.php
@@ -3,11 +3,14 @@
 $page = get_page_by_title( 'About' );
 $content = $page->post_content;
 $trimmed_content = wp_trim_words( $content, 20, null );
+$featured_image = wp_get_attachment_url( get_post_thumbnail_id($page->ID) );
 ?>
 <div id="featuredPage">
 
 	<h4><a href="<?php echo get_the_permalink($page->ID); ?>">What is Critical Global Health?</a></h4>
-	<p><?php echo $trimmed_content; ?></p>
+    <p style="background: url('<?php echo $featured_image;?>'); background-size: cover;"><span><?php echo $trimmed_content; ?></span></p>
+
+
 
 </div>
 
--- partials/loop-latest-posts.php
+++ partials/loop-latest-posts.php
@@ -4,8 +4,13 @@ $lastposts = get_posts( $args ); ?>
 <div id="latestContainer">
 <?php
 foreach($lastposts as $post) : setup_postdata($post); ?>
+    <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
 	<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-	<p><?php the_post_thumbnail();?></p>
+    <?php
+$feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+
+?>
+    <p style="background: url('<?php echo $feat_image;?>'); background-size: cover;"><span><?php echo wp_strip_all_tags( get_the_content()); ?></span></p>
 
 
 <?php endforeach; ?>
