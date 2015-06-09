c42d7eb99d60d1b4222ca0a64ea6c08b72078518
alastair38 <alastair@alastaircox.com>
2015-06-09 21:28:09 +0100

    Version 1.3.3 - bunch of IE8/9 fixes.

--- partials/loop-slider.php
+++ partials/loop-slider.php
@@ -1,7 +1,7 @@
         <?php
         $firstImage = get_field('first_image');
         if( !empty($firstImage) ): ?>
-        <div style="background: url('<?php echo $firstImage['url']; ?>'); background-size: cover;">
+        <div style="background: url('<?php echo $firstImage['url']; ?>'); background-size: cover; background-position: 100% 100%; ">
                 <p><em><a href="<?php echo $firstImage['caption']; ?>" target="_blank"><?php echo $firstImage['alt']; ?></a></em></p>
         </div>
         <?php endif; ?>
@@ -9,7 +9,7 @@
         <?php
         $secondImage = get_field('second_image');
         if( !empty($secondImage) ): ?>
-        <div style="background: url('<?php echo $secondImage['url']; ?>'); background-size: cover;">
+        <div style="background: url('<?php echo $secondImage['url']; ?>'); background-size: cover; background-position: 100% 100%;">
                  <p><em><a href="<?php echo $secondImage['caption']; ?>" target="_blank"><?php echo $secondImage['alt']; ?></a></em></p>
         </div>
         <?php endif; ?>
@@ -17,7 +17,7 @@
         <?php
         $thirdImage = get_field('third_image');
         if( !empty($thirdImage) ): ?>
-        <div style="background: url('<?php echo $thirdImage['url']; ?>'); background-size: cover;">
+        <div style="background: url('<?php echo $thirdImage['url']; ?>'); background-size: cover; background-position: 100% 100%;">
                  <p><em><a href="<?php echo $thirdImage['caption']; ?>" target="_blank"><?php echo $thirdImage['alt']; ?></a></em></p>
         </div>
         <?php endif; ?>
