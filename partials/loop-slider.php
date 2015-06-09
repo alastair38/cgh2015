ebdfdb58704eea883727e84eca14493b21126ac7
alastair38 <alastair@alastaircox.com>
2015-06-09 13:52:23 +0100

    Version 1.3.1 - added caption option to loop-slider to enable attribution, when required. Minor margin change to .home .entry-content

--- partials/loop-slider.php
+++ partials/loop-slider.php
@@ -2,7 +2,7 @@
         $firstImage = get_field('first_image');
         if( !empty($firstImage) ): ?>
         <div style="background: url('<?php echo $firstImage['url']; ?>'); background-size: cover;">
-
+                <p><em><a href="<?php echo $firstImage['caption']; ?>" target="_blank"><?php echo $firstImage['title']; ?></a></em></p>
         </div>
         <?php endif; ?>
 
@@ -10,7 +10,7 @@
         $secondImage = get_field('second_image');
         if( !empty($secondImage) ): ?>
         <div style="background: url('<?php echo $secondImage['url']; ?>'); background-size: cover;">
-
+                 <p><em><a href="<?php echo $secondImage['caption']; ?>" target="_blank"><?php echo $secondImage['title']; ?></a></em></p>
         </div>
         <?php endif; ?>
 
@@ -18,6 +18,6 @@
         $thirdImage = get_field('third_image');
         if( !empty($thirdImage) ): ?>
         <div style="background: url('<?php echo $thirdImage['url']; ?>'); background-size: cover;">
-
+                 <p><em><a href="<?php echo $thirdImage['caption']; ?>" target="_blank"><?php echo $thirdImage['title']; ?></a></em></p>
         </div>
         <?php endif; ?>
